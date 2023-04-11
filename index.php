839fc3d4affa8229af513618016fcc0587b5a34a
栗 <121975413+lizina66@users.noreply.github.com>
2023-04-11 11:45:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
 <?php
-//txt文件位置
-const DBFILE = 'img.txt';
-
-//读取数据 有SSL可＋S
-$data = file(DBFILE, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-if (empty($data[0])){
-    header('HTTP/1.1 503 Service Unavailable');
-    die ('503 Service Unavailable');
-}
-
-header('Access-Control-Max-Age: 86400');
-header('Access-Control-Allow-Origin: *');
-$id = $_REQUEST['id'];
-
-if(!isset($id)){
-    $id = array_rand($data) + 1;
-}
-settype($id,'integer');
-if ($id <= 0 || $id > $quantity){
-    $id = array_rand($data) + 1;
-}
-
-$pic = $data[$id - 1];
-switch ($type) {
-    case 'quantity':
-        echo $quantity;
-        break;
-    case 'json':
-        $result = [
-            'code' => 200,
-            'id' => $id,
-            'url' => $pic
-        ];
-        header('Content-Type: text');
-        echo json_encode($result);
-        break;
-    default:
-        header("Location:" . $pic);
-}
-
+$img_array = glob("img/*.{gif,jpg,png}",GLOB_BRACE); 
+$img = array_rand($img_array);
+$dz = $img_array[$img];
+header("Location:".$dz);
 ?>
